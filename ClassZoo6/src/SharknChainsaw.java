89bc0fc35ab2116ec1d15f49f159305db7573f7e
telepatheticman <telepatheticman13@gmail.com>
2015-01-07 13:50:32 -0600

    added chainsaw shark. no really. I did.

---
+++ ClassZoo6/src/SharknChainsaw.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.applet.*;
+import java.util.Random;
+public class SharknChainsaw extends Applet{
+	public void paint(Graphics g){
+		g.setColor(Color.blue);
+	}
+}
+class fish{
+	
+}
+class Delay{
+	public void delay(){
+		
+	}
+}
