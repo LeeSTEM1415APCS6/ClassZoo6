81ed45dfbca0b8e1702d5911e577927a4a28fd10
telepatheticman <telepatheticman13@gmail.com>
2015-01-08 13:24:02 -0600

    push

--- ClassZoo6/src/SharknChainsaw.java
+++ ClassZoo6/src/SharknChainsaw.java
@@ -4,13 +4,17 @@ import java.util.Random;
 public class SharknChainsaw extends Applet{
 	public void paint(Graphics g){
 		g.setColor(Color.blue);
+		g.fillRect(0, 0, 800, 800);
 	}
 }
 class fish{
 	
 }
 class Delay{
-	public void delay(){
-		
+	public void delay(int n){
+		long startDelay = System.currentTimeMillis();
+		long endDelay = 0;
+		while (endDelay - startDelay < n)
+			endDelay = System.currentTimeMillis();	
 	}
 }
